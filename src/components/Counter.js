57262b8e56a2b62061987142c7bb962ad01eb971
BriannaLoveless <briannabel97@gmail.com>
2022-02-15 12:39:55 -0600

    all buttons affect the ability score & are capped between 8 & 15

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,11 +9,20 @@ export default function Counter(props){
                 <div className="d-flex flex-column card-body p-3" style={{ position: 'relative' }}>
                     <div className="d-flex align-items-center justify-content-center">
                         <h3 className="text-center px-4">{`${props.value}`}</h3>
-                        <button className="btn btn-sm" style={{ position: 'absolute', right: 10}}>+</button>
+                        <button 
+                            className="btn btn-sm" 
+                            style={{ position: 'absolute', right: 10}} 
+                            onClick={()=>props.increment(props.id)}
+                            
+                        >+</button>
                     </div>
                     <div className="d-flex align-items-center justify-content-center">
                         <div className="text-center px-4">{`${props.stat}`}</div>
-                        <button className="btn btn-sm" style={{ position: 'absolute', right: 10}}>-</button>
+                        <button 
+                            className="btn btn-sm" 
+                            style={{ position: 'absolute', right: 10}} 
+                            onClick={()=>props.decrement(props.id)}
+                        >-</button>
                     </div>
                 </div>
             </div>
